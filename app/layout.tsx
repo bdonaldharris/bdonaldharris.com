25eb7e1d1ee1d06978cc3b3f88011221962b34ab
NotableBIT <bdonaldharris@outlook.com>
2026-07-28 18:56:28 -0500

    Load indigo theme palette

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import "./theme-palette.css";
 import { SiteFooter } from "@/components/layout/site-footer";
 import { SiteHeader } from "@/components/layout/site-header";
 
